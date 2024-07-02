265601b84da837b1ada8059e0574af72a873f76e
Ayomide <vayomide2@gmail.com>
2024-07-02 21:03:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import UserInput from "./components/userInput"
+import { useState } from "react"
+import Result from "./components/result"
+function App() {
+  const [userInput, setuserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+})
+function handleChange(inputIdentifier, newValue) {
+    setuserInput((prevUserInput) => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: +newValue,
+        }
+    })
+}
+  
+
+  const isInputValid = userInput.duration >= 1
+  
+  return (
+    <div>
+      <header id="header">
+        <img src="../src/assets/investment-calculator-logo.png" alt="logo" />
+        <h1>Investment calculator</h1>
+      </header>
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {isInputValid ? <Result input={userInput} /> : <p className="center">Duration must not be less than 1</p>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/userInput.jsx
@@ -0,0 +1,29 @@
+
+
+export default function UserInput({onChange, userInput}) {
+    
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="initial-investment">Initial Investment</label>
+          <input type="number" required value={userInput.initialInvestment} onChange={(e)=>onChange('initialInvestment', e.target.value)}/>
+        </p>
+        <p>
+          <label htmlFor="initial-investment">Annual Investment</label>
+          <input type="number" required value={userInput.annualInvestment} onChange={(e)=>onChange('annualInvestment', e.target.value)}/>
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">Expected Return</label>
+          <input type="number" required value={userInput.expectedReturn} onChange={(e)=>onChange('expectedReturn', e.target.value)}/>
+        </p>
+        <p>
+          <label htmlFor="duration">Duration</label>
+          <input type="number" required value={userInput.duration} onChange={(e)=>onChange('duration', e.target.value)}/>
+        </p>
+      </div>
+    </div>
+  );
+}
